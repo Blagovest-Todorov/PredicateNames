626f57cd8a13436b10be4ec13fed9fa194cf3582
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-06-06 09:51:22 +0300

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PredicateNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+
+            List<string> names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Action<List<string>> printNames = namesList => namesList
+            .Where(el => el.Length <= length)
+            .ToList()
+            .ForEach(el => Console.WriteLine(el));
+
+            printNames(names);
+        }       
+    }
+}
